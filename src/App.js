f1b9377ff87f9dc09a08e525c2d90f4a2356566a
Cristian Morataya <cris.morataya@gmail.com>
2022-06-17 22:25:24 -0600

    To deploy with amplify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>CM:: my react app</p>
+        <strong>To deploy with amplify</strong>
+        &nbsp;
         <a
           className="App-link"
           href="https://reactjs.org"
